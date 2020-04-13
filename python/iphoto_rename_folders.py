5192e15ff5180f8d378ee435b0612f9539c3992b
valch85 <val@valch.name>
2020-04-14 02:25:54 +0300

    add func date catching and formating

--- python/iphoto_rename_folders.py
+++ python/iphoto_rename_folders.py
@@ -19,10 +19,17 @@ def get_loc(dir_name):
     return location
 
 def get_date(dir_name):
-    day = re.search('[a-zA-Z0-9_ ]*,\s(\d{1,2}).*', dir_name).group(0)
-    month = re.search('January|February|March|April|May|June|July|August|September|October|November|December', dir_name).group(0)
+    day = re.search('[a-zA-Z0-9_ ]*,\s(\d{1,2}).*', dir_name).group(1)
+    if len(str(day)) == 1:
+        day = "0"+str(day)
+    month_letter = re.search('January|February|March|April|May|June|July|August|September|October|November|December', dir_name).group(0)
+    month_digit = list(calendar.month_abbr).index(month_letter[:3])
+    if len(str(month_digit)) == 1:
+        month_digit = "0"+str(month_digit)
+
     year = re.search('\d+$', dir_name).group(0)
-    print('day = ' +str(day) + ' month = ' +str(month) + ' year = ' +str(year))
+    date = str(year)+'-'+str(month_digit)+'-'+str(day)
+    return date
 
 # path to dirrectory
 folder_path = sys.argv[1]
